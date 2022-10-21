9a8058469786ac3478979a3f746590a33449d36e
MARTINA1102 <martinamiccoli11@gmail.com>
2022-10-21 17:53:06 +0200

    primo commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const chilometri = document.querySelector('[name=chilometri]').value;
+const nomeCognome=document.querySelector('[name=nomecogn]').value;
+const eta = document.querySelector('[name=età]').value;
+const centesimiPerKm = 0.21;
+const prezzoBigliettoPerKm = chilometri * centesimiPerKm ;
+let sconto= 0;
+const elebutton= document.querySelector ('.btn');
+const bigliettoFinale= document.querySelector ('.demo');
+let prezzoFinale=Math.round((prezzoBigliettoPerKm - (prezzoBigliettoPerKm * sconto)) * 100) / 100;
+    console.log(prezzoFinale);
+let nomePasseggero= document.querySelector ('nome_pass');
+let numeroCasuale1=document.querySelector ('.numero-casuale-1');
+let numeroCasuale2=document.querySelector ('.numero-casuale-2');
+let prezzoFinaleTot=document.querySelector ('prezzo-finale-biglietto');
+
+if (eta < 18){
+    let sconto=0.20;
+   
+}else if (eta > 65){
+    let sconto=0.40;
+
+}
+    
+
+elebutton.addEventListener('click',function(){
+    bigliettoFinale.classList.toggle('show'); 
+});
+nomePasseggero.innerHTML=nomeCognome;
+numeroCasuale1.innerHTML=Math.floor(Math.random() * 11);
+numeroCasuale2.innerHTML=Math.floor(Math.random() * 100001);
+prezzoFinaleTot.innerHTML=prezzoFinale;
+
+

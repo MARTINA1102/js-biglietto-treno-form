6f9b8bcf2f8fe98c95e1f06f43737efb3c20e102
MARTINA1102 <martinamiccoli11@gmail.com>
2022-10-23 09:38:57 +0200

    calcolo funzionante

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
 const chilometri = document.querySelector('[name=chilometri]').value;
-const nomeCognome=document.querySelector('[name=nomecogn]');
 const eta = document.querySelector('[name=età]').value;
 const centesimiPerKm = 0.21;
 const prezzoBigliettoPerKm = chilometri * centesimiPerKm ;
-let sconto= 0;
 const elebutton= document.querySelector ('.btn');
 const bigliettoFinale= document.querySelector ('.demo');
-let prezzoFinale=Math.round((prezzoBigliettoPerKm - (prezzoBigliettoPerKm * sconto)) * 100) / 100;
-    console.log(prezzoFinale);
 let nomePasseggero= document.querySelector ('nome_pass');
 let numeroCasuale1=document.querySelector ('.numero-casuale-1');
 let numeroCasuale2=document.querySelector ('.numero-casuale-2');
 let prezzoFinaleTot=document.querySelector ('prezzo-finale-biglietto');
 
-if (eta < 18){
-    let sconto=0.20;
-   
-}else if (eta > 65){
-    let sconto=0.40;
 
-}
     
 
 elebutton.addEventListener('click',function(){
-    bigliettoFinale.classList.toggle('show'); 
-});
+    bigliettoFinale.classList.toggle('show');
+    const chilometri = document.querySelector('[name=chilometri]').value;
+    const eta = document.querySelector('[name=età]').value;
+    const centesimiPerKm = 0.21;
+    const prezzoBigliettoPerKm = chilometri * centesimiPerKm ;
 
-numeroCasuale1.innerHTML=Math.floor(Math.random() * 11);
-numeroCasuale2.innerHTML=Math.floor(Math.random() * 100001);
-prezzoFinaleTot.innerHTML=prezzoFinale;
+    if (eta=== 'min'){
+        let sconto=0.20;
+        let prezzoFinale=prezzoBigliettoPerKm - prezzoBigliettoPerKm * sconto / 100;
+        prezzoFinale= parseFloat(prezzoFinale.toFixed(2));
+        console.log(prezzoFinale);
+        prezzoFinaleTot=prezzoFinale
+    
+       
+    }else if (eta === 'over'){
+        let sconto=0.40;
+        let prezzoFinale=prezzoBigliettoPerKm - prezzoBigliettoPerKm * sconto / 100;
+        prezzoFinale= parseFloat(prezzoFinale.toFixed(2));
+        console.log(prezzoFinale);
+    
+    
+    }else{
+        let sconto=0;
+        let prezzoFinale=prezzoBigliettoPerKm - prezzoBigliettoPerKm * sconto / 100;
+        prezzoFinale= parseFloat(prezzoFinale.toFixed(2));
+        console.log(prezzoFinale);
+    
+    }
+
+    numeroCasuale1.innerHTML=Math.floor(Math.random() * 11);
+    numeroCasuale2.innerHTML=Math.floor(Math.random() * 100001);
+    nomePasseggero= document.querySelector('[name=nomecogn]');
+    
+})
+
 
 
